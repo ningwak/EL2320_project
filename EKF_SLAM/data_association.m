03034be3af8ea707009e42525d129696b7f5c324
Ning Wang <41559363+lxpr@users.noreply.github.com>
2021-12-18 21:34:47 +0100

    Delete data_association.m

--- EKF_SLAM/data_association.m
+++
@@ -1,9 +0,0 @@
-% This function performs the data association.
-% Inputs:
-%           mu_bar(t)             (3N + 3) x 1   
-%           sigma_bar(t)          (3N + 3) x (3N + 3)
-%           z                     Current observation (not associated)
-% Outputs:   
-%           z                     Current observation (associated)
-function z = data_association(mu_bar, sigma_bar, z)
-
