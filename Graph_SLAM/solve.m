6e0a57c96f4961697ee51b393cdb66221b43e001
wgz0320 <96132476+wgz0320@users.noreply.github.com>
2021-12-20 22:44:17 +0800

    Add files via upload

--- Graph_SLAM/solve.m
+++ Graph_SLAM/solve.m
@@ -1,7 +1,7 @@
-function [x,map,cov] = solve(omega_hat,xi_hat,omega,xi,tau,x,m)
+function [pose,map,cov] = solve(omega_hat,xi_hat,omega,xi,tau,x,m)
 
 cov=inv(omega_hat);
-x=cov*xi_hat;
+pose=cov*xi_hat;
 map=zeros(3*size(m,2),1);
 
 %for each feature do
@@ -9,11 +9,11 @@ for i=1:size(m,2)
     index_j = find_information_index(x, i, "map");
     index_tauj=find_information_index(x, tau{i}, "car");
     
-    map((3*i-2):(3*i)) = (omega(index_j, index_j)) \ (xi(index_j) + omega(index_j, index_tauj) * x(index_tauj));
+    map((3*i-2):(3*i)) = (omega(index_j, index_j)) \ (xi(index_j) + omega(index_j, index_tauj) * pose(index_tauj));
    
 end
 
-x = reshape(x, 3, []);
+pose = reshape(pose, 3, []);
 map = reshape(map, 3, []);
 
 fprintf("solve successfully\n");
